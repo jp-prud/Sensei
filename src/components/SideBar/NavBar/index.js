488ab2fbfbc3d680d6fca6df4f03211adf4c8b26
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2021-12-08 22:03:38 -0300

    Feat: Criando a Navbar e Rotas

---
+++ src/components/SideBar/NavBar/index.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container } from './style';
+
+import Home from '../../../assets/images/icons/sidebar/Home.svg';
+import Post from '../../../assets/images/icons/sidebar/Document.svg';
+import Folder from '../../../assets/images/icons/sidebar/Folder.svg';
+import Image from '../../../assets/images/icons/sidebar/Image.svg';
+import Paper from '../../../assets/images/icons/sidebar/Paper.svg';
+import Message from '../../../assets/images/icons/sidebar/Message.svg';
+import Edit from '../../../assets/images/icons/sidebar/Edit.svg';
+import Extension from '../../../assets/images/icons/sidebar/Extension.svg';
+import Peoples from '../../../assets/images/icons/sidebar/Peoples.svg';
+import Setting from '../../../assets/images/icons/sidebar/Setting.svg';
+
+export default function Navbar({ isActive }) {
+  return (
+    <Container>
+      <Link to="/" isActive={isActive}>
+        <img src={Home} alt="Home Page" />
+        <span>Dashboard</span>
+      </Link>
+      <Link to="/post">
+        <img src={Post} alt="Post Page" />
+        <span>Post</span>
+      </Link>
+      <Link to="/categories">
+        <img src={Folder} alt="Category Page" />
+        <span>Categories</span>
+      </Link>
+      <Link to="/media">
+        <img src={Image} alt="Media Page" />
+        <span>Media</span>
+      </Link>
+      <Link to="/pages">
+        <img src={Paper} alt="Pages" />
+        <span>Pages</span>
+      </Link>
+      <Link to="/comments">
+        <img src={Message} alt="Comments Page" />
+        <span>Comments</span>
+      </Link>
+
+      <span className="section-title"> System </span>
+
+      <Link to="/apparance">
+        <img src={Edit} alt="Edit Page" />
+        <span>Apparance</span>
+      </Link>
+      <Link to="/extensions">
+        <img src={Extension} alt="Extension Page" />
+        <span>Extensions</span>
+      </Link>
+      <Link to="/users">
+        <img src={Peoples} alt="Peoples Page" />
+        <span>Users</span>
+      </Link>
+      <Link to="/settings">
+        <img src={Setting} alt="Setting Page" />
+        <span>Settings</span>
+      </Link>
+    </Container>
+  );
+}
---
+++ src/components/SideBar/NavBar/style.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styled.nav`
+  display: flex;
+  flex-direction: column;
+
+  a {
+    height: 51px;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    opacity: 0.5;
+    color: ${({ theme }) => theme.colors.white};
+    transition: all .3s ease-out;
+
+    :hover {
+      opacity: 1
+    }
+
+    img {
+      margin-right: 10px;
+    }
+  }
+
+  .section-title {
+    display: block;
+    height: 30px;
+    text-transform: uppercase;
+    opacity: 0.5;
+    color: ${({ theme }) => theme.colors.white};
+    font: ${({ theme }) => theme.font.body};
+  }
+`;
