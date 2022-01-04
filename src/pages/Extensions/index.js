bf655b1352878ca47e90934b07011427b031b094
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-01-04 16:04:21 -0300

    Feat: Página de Extensões

--- src/pages/Extensions/index.js
+++ src/pages/Extensions/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import ExtensionCard from '../../components/ExtensionCard';
 
 import PageHeader from '../../components/PageHeader';
 
+import { Container } from './style';
+
 export default function Extensions() {
   return (
-    <PageHeader title="Extensões" button />
+    <>
+      <PageHeader title="Extensões" button />
+
+      <Container>
+        <ExtensionCard />
+      </Container>
+    </>
   );
 }
