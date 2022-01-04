0228e34d74b4a1006121dd3af1eac5e567739761
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-01-04 16:03:11 -0300

    Feat: Componentização para select

--- src/components/select.js
+++ src/components/select.js
@@ -3,9 +3,9 @@ import styled from 'styled-components';
 export default styled.select`
   height: 44px;
   width: 100%;
-  border: 2px solid #FFF;
-  background: ${({ theme }) => theme.inputBackground};
-  color: ${({ theme }) => theme.colors.gray};
+  border: 2px solid ${({ theme }) => theme.colors.border};
+  background: ${({ theme }) => theme.colors.white};
+  color: ${({ theme }) => theme.colors.label};
   border-radius: 8px;
   padding: 0 16px;
   transition: border-color .3s ease-in;
