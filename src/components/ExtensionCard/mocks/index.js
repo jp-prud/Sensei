b9ef923fb97a8293e6c1672e375873df0bc53d3c
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-01-04 16:05:25 -0300

    Feat: Mocks para as extensões

---
+++ src/components/ExtensionCard/mocks/index.js
@@ -0,0 +1,38 @@
+import TinyLogo from '../../../assets/images/extensions/tiny-logo.png';
+import MercadoPagoLogo from '../../../assets/images/extensions/mercado-pago-logo.webp';
+import PagarMeLogo from '../../../assets/images/extensions/pagarme-logo.webp';
+import EbitLogo from '../../../assets/images/extensions/ebit-logo.webp';
+import SmarthintLogo from '../../../assets/images/extensions/smarthint-logo.webp';
+
+export default [
+  {
+    image: TinyLogo,
+    title: 'Tiny ERP',
+    description: 'Com o Tiny você padroniza os processos aumenta a eficiência da operação e gerencia os principais recursos do seu negócio.',
+    status: 'Ativo',
+  },
+  {
+    image: MercadoPagoLogo,
+    title: 'Mercado Pago',
+    description: 'Aproveite a tecnologia do Mercado Pago para vender na sua loja online e receber pagamentos de maneira segura',
+    status: 'Ativo',
+  },
+  {
+    image: PagarMeLogo,
+    title: 'Pagar.me',
+    description: 'O Pagar.me é a forma mais simples de realizar vendas online com toda segurança e transparência',
+    status: 'Ativo',
+  },
+  {
+    image: EbitLogo,
+    title: 'Ebit',
+    description: 'Os melhores sistemas de conversão em um só. Melhore a experiência do seu site e venda mais.',
+    status: 'Ativo',
+  },
+  {
+    image: SmarthintLogo,
+    title: 'SmatHint',
+    description: 'Potencialize a experência de compra com a personalização do seu e-comeerce. Oferte os produtos certos para o público e venda mais.',
+    status: 'Ativo',
+  },
+];
