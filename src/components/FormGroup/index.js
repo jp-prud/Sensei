c8bca7e2ea621a8bf512ab93a23b23f38d77f344
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-01-04 16:13:47 -0300

    Feat: Grupo para formulario

--- src/components/FormGroup/index.js
+++ src/components/FormGroup/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 import { Container } from './styled';
 
-export default function FormGroup({ children }) {
+export default function FormGroup({ children, error }) {
   return (
     <Container>
       {children}
+
+      {error && (
+        <small>
+          {error}
+        </small>
+      )}
     </Container>
   );
 }
