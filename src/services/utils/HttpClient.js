391d6a7a041e10a295835ad9a50ca0d38ff56b11
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-01-13 23:09:51 -0300

    Feat: Tratamento de erros no HttpClient

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -6,11 +6,23 @@ class HttpClient {
   }
 
   async get(path) {
-    const response = await fetch(`${this.baseURL}${path}`);
-
     await delay(1000);
 
-    return response.json();
+    const response = await fetch(`${this.baseURL}${path}`);
+    const contentType = response.headers.get('Content-Type');
+    let body = null;
+
+    if (contentType.includes('application/json')) {
+      body = await response.json();
+    }
+
+    if (response.ok) {
+      return body;
+    }
+
+    throw new Error(
+      body?.error || `${response.status - response.statusText}`,
+    );
   }
 }
 
