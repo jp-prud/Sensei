98c71c5faf0ba56b5530412f642f787915cbcd2b
João Pedro Prudêncio <joaopprudencio82@gmail.com>
2022-01-13 23:10:17 -0300

    Fix: Unificando tratativas de erro

--- src/pages/Users/index.js
+++ src/pages/Users/index.js
@@ -43,7 +43,7 @@ export default function Users() {
 
         setClients(listUsers);
       } catch (error) {
-        throw new Error(error);
+        console.log(error);
       } finally {
         setIsLoading(false);
       }
